662bd2e70be5468955a6ce953c043e6d075c6dfb
Lindsay <lbtannahill@gmail.com>
2022-02-24 12:57:33 -0500

    added DayList and DayListItems components - creating the css and framing

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,20 @@
 import React from "react";
-
+import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
+   const buttonClass = classNames("button", {
+     "button--confirm": props.confirm,
+     "button--danger": props.danger
+   });
  
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
-   return <button className={buttonClass}>{props.children}</button>;
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
  }
