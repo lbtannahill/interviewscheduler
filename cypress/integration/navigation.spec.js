88a6ff580e4c2a961ba847b55549e38aff3dc0cd
Lindsay <lbtannahill@gmail.com>
2022-03-04 09:55:33 -0800

    finished cypress tests

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,4 +6,9 @@ describe("Navigation", () => {
 
 it("should navigate to Tuesday", () => {
   cy.visit("/");
+
+  cy.contains("li", "Tuesday")
+  .click()
+  .should("have.css", "background-color", "rgb(242, 242, 242)");
 });
+
